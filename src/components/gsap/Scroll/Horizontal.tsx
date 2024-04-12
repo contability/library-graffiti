c057822f7d9db8c13eaab4521753912cec8c8940
furium <shouldBnewB@gmail.com>
2024-04-12 12:10:25 +0900

    (add) gsap horizontal scroll css

--- src/components/gsap/Scroll/Horizontal.tsx
+++ src/components/gsap/Scroll/Horizontal.tsx
@@ -1,12 +1,15 @@
-import { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect, useRef } from "react";
 import { styled } from "styled-components";
+gsap.registerPlugin(ScrollTrigger);
 
 const GsapScrollHorizontalContainer = styled.article`
   overflow-y: auto;
-  /* overflow-scrolling: touch; */
-  -webkit-overflow-scrolling: touch;
+  overflow-x: hidden;
   color: black;
   font-size: 2.5rem;
+  position: relative;
 
   .section-first {
     display: flex;
@@ -62,14 +65,35 @@ const GsapScrollHorizontalContainer = styled.article`
 `;
 
 const GsapScrollHorizontal = () => {
-  useEffect(() => {}, []);
+  const component = useRef<HTMLElement>(null);
+  const slider = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const panels = gsap.utils.toArray(".panel");
+      gsap.to(panels, {
+        xPercent: -100 * (panels.length - 1),
+        ease: "none",
+        scrollTrigger: {
+          trigger: slider.current,
+          pin: true,
+          scrub: 1,
+          snap: 1 / (panels.length - 1),
+          end: () => "+=" + slider.current?.offsetWidth,
+          markers: true,
+        },
+      });
+    }, component);
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <GsapScrollHorizontalContainer>
+    <GsapScrollHorizontalContainer ref={component}>
       <section className="section-first">
         <h1>Testing horizontal scrolling w/ three sections</h1>
         <h2>First Container</h2>
       </section>
-      <section className="section-scroll__horizontal">
+      <section className="section-scroll__horizontal" ref={slider}>
         <div className="description panel blue">
           <div>
             SCROLL DOWN
